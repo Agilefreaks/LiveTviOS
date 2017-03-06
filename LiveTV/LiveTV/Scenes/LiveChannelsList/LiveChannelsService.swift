04562db88063b3d72202a5b7cc5fa1bf4ec4fe2a
Mihai Pantiru <mihai.pantiru@gmail.com>
2017-03-06 18:27:25 +0200

    [lc-14] Architecture work + first unit test.

---
+++ LiveTV/LiveTV/Scenes/LiveChannelsList/LiveChannelsService.swift
@@ -0,0 +1,21 @@
+//
+//  LiveChannelsService.swift
+//  LiveTV
+//
+//  Created by Mihai Pantiru on 06/03/2017.
+//  Copyright © 2017 AgileFreaks. All rights reserved.
+//
+
+import Foundation
+
+protocol ListChannelsServiceProtocol {
+    func getLiveChannelsList(success: @escaping ([LiveChannel]) -> Void, failure: @escaping (Error) -> Void)
+}
+
+class LiveChannelsService: ListChannelsServiceProtocol {
+    func getLiveChannelsList(success: @escaping ([LiveChannel]) -> Void, failure _: @escaping (Error) -> Void) {
+        // Dummy
+        let data = LiveChannel(name: "test", logoUrl: "test", streamingUrl: "test", title: "test")
+        success([data])
+    }
+}
